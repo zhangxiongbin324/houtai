df6ebe6c11f8d80e356f7978c4808a4e1e40277c
实训 2 <1501628763@qq.com>
2016-09-25 19:52:40 +0800

    葛晓敏
    葛晓敏

--- app/scripts/controllers/shouye.js
+++ app/scripts/controllers/shouye.js
@@ -7,53 +7,57 @@
  * # MainCtrl
  * Controller of the App
  */
-var server = "http://123.56.227.177:2503"
 angular.module('App')
-	.controller('shouye', function($scope, $http) {
-		$http({
-			method: "GET",
-			url: server + "/jing-ex/"
-		}).success(function(e) {
-			$scope.data = e
+	.controller('shouye',function($scope, $http){
+		$scope.s={}
+	  var server="http://123.56.227.177:2503";
+            $http({
+                method:"GET",
+                url:server +"/jing-ex"
+            }).success(function(e){
+                $scope.data=e
+            })
+            $scope.edit=function(e){
+                $scope.f=true
+                $scope.s=e
+                var content=$scope.s.content;
+     		   	$('.abc').summernote('code',content)
+            }
+            $scope.save=function(){  
+            	$scope.s.content =$(".abc").summernote("code");
+                $http({
+                    method:"PUT",
+                    url:server+"/jing-ex/"+$scope.s.id,
+                    data:$scope.s
+                }).success(function(){
+                	$scope.f = false;
+                })
+            }
+            $scope.addsave=function(){
+            	console.log($(".abc").summernote("code"))
+                $scope.s.content=$(".abc").summernote("code")
+                $http({
+                    url:server+"/jing-ex/",
+                    method:"POST",
+                    data:$scope.s
+                }).success(function(){
+                    $scope.data.push($scope.s)
+                })
+            }
+            $scope.del=function(e){
+                $http({
+                    url:server+"/jing-ex/"+ e.id,
+                    method:"DELETE"
+                }).success(function(){
+                    $scope.data.splice($scope.data.indexOf(e),1)
+                })
+            } 
+            $scope.tj=function(){
+            	$scope.f=true
+            }
+            $scope.yc=function(){
+            	$scope.f=false;
+            }
 		})
 
-		$scope.Edit = function(e) {
-			$scope.isshow = true;
-			$scope.c = e;
-		}
-
-		$scope.Required = function() {
-			$http({
-				method: "PUT",
-				url: server + "/jing-ex/" + $scope.c.id,
-				data: $scope.c
-			}).success(function() {
-				$scope.isshow = false;
-			})
-		}
-
-		$scope.Add = function() {
-			$scope.isshow = true
-		}
-
-		$scope.AddRequired = function() {
-			$http({
-				method: "POST",
-				url: server + "/jing-ex/",
-				data: $scope.c
-			}).success(function() {
-				$scope.data.push($scope.c);
-				$scope.isshow = false;
-				$scope.c = ''
-			})
-		}
-
-		$scope.Del = function(e) {
-			$http({
-				method: "DELETE",
-				url: server + "/jing-ex/" + e.id
-			}).success(function() {
-				$scope.data.splice($scope.data.indexOf(e), 1)
-			})
-		}
-	});
+	
